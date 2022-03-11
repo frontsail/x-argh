759b7bbcf38f4d52dc877992d5c2b11896cfff24
Muris Ceman <murisceman@gmail.com>
2022-03-11 19:58:27 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 export default function (Alpine) {
-  Alpine.directive('argh', (el, { value, modifiers, expression }, { effect, evaluateLater }) => {
+  // The directive
+  Alpine.directive('argh', (el, { value, modifiers, expression }, { effect, evaluate }) => {
+    // Value is
+    // required
     if (!value) {
       return
     }
 
+    // @todo need better var names, comments, and documentation
+
     let { argObj, argName, deep } = resolve(el._x_dataStack, expression)
     const propObj = el._x_dataStack[0]
     const propName = camelize(value)
-    const evaluator = argObj ? null : evaluateLater(expression)
     const bind = argObj && modifiers.includes('bind')
 
     let prevPropValue = propObj[propName]
+    let prevArgObj = argObj
     let prevArgValue = undefined
 
     effect(() => {
+      prevArgObj = argObj
       deep && ({ argObj, argName, deep } = resolve(el._x_dataStack, expression))
 
       if (argObj) {
         // One-way binding
-        if (argObj[argName] !== prevArgValue) {
+        if (argObj[argName] !== prevArgValue || argObj !== prevArgObj) {
           propObj[propName] = argObj[argName]
         }
 
@@ -30,22 +36,38 @@ export default function (Alpine) {
         prevArgValue = argObj[argName]
         prevPropValue = propObj[propName]
       } else {
-        // The evaluator is used in case that the argument is not a property
-        // of the parent component data (one-way binding).
-        evaluator((argValue) => {
-          if (argValue !== prevArgValue) {
-            propObj[propName] = argValue
-            prevArgValue = argValue
-          }
-        })
+        // The evaluator is used in
+        // case that the argument is
+        // not a property of the parent
+        // component's data (one-way binding).
+        const argValue = evaluate(expression)
+
+        if (argValue !== prevArgValue) {
+          propObj[propName] = argValue
+          prevArgValue = argValue
+        }
       }
     })
   })
 
+  /**
+   * Transforms an example-string into an exampleString.
+   *
+   * @param {string} s String to camelize
+   * @returns {string} The camelized string
+   */
   function camelize(s) {
     return s.replace(/-./g, (x) => x[1].toUpperCase())
   }
 
+  /**
+   * Find the closest property from an expression by searching Alpine data
+   * stacks of the parent components.
+   *
+   * @param {object} stack Alpine data stack
+   * @param {string} expression The directive attribute value
+   * @returns {object} Resolved argument parameters
+   */
   function resolve(stack, expression) {
     let argObj = null
     let argName = null
@@ -63,14 +85,10 @@ export default function (Alpine) {
           .reduce((o, i) => (typeof o === 'object' ? o[i] : false), stack[i])
 
         if (obj) {
-          const name = dotNotation.slice(-1)
-
-          if (name in obj) {
-            argObj = obj
-            argName = name[0]
-            deep = obj !== stack[i]
-            break
-          }
+          argObj = obj
+          argName = dotNotation[dotNotation.length - 1]
+          deep = obj !== stack[i]
+          break
         }
       }
     }
